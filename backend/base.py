5635019a3be9fdfe78f07a9b3d68fe080e39df82
shravan1996 <shravanbittu2013@gmail.com>
2023-06-28 11:09:07 +0530

    package

--- backend/base.py
+++ backend/base.py
@@ -1,17 +1,3 @@
-#from flask import Flask
-
-#api = Flask(__name__)
-
-#@api.route('/profile')
-#def my_profile():
-#    response_body = {
-#        "name": "Nagato",
-#        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-#    }
-
-#   return response_body
-
-
 from flask import Flask, request, send_file
 import io
 import torch
